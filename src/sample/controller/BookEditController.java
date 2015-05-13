2dd8027272d61ec496ba83b4507a136832a55591
Gorobzeev <staszp@gmail.com>
2015-05-14 02:37:57 +0300

    add book_edit

---
+++ src/sample/controller/BookEditController.java
@@ -0,0 +1,21 @@
+package sample.controller;
+
+import sample.Books.Book;
+
+/**
+ * Created by Admin on 14.05.2015.
+ */
+public class BookEditController {
+
+
+
+
+
+
+
+
+    public void setBook(Book book){
+
+
+    }
+}
